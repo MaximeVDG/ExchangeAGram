153ac8b9c134a0fda623012bf454c2e16b524e74
MaximeVDG <Max@MacBook-Air-de-Maxime.local>
2015-01-06 13:05:23 +0900

    added placeholder cells in FilterVC

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -26,6 +26,8 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.backgroundColor = UIColor.whiteColor()
+        collectionView.registerClass(FilterCell.self, forCellWithReuseIdentifier: "MyCell")
+        
         self.view.addSubview(collectionView)
     }
 
@@ -35,11 +37,15 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 2
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        
+        let cell: FilterCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as FilterCell
+        cell.imageView.image = UIImage(named: "Placeholder")
+        
+        return cell
     }
 
 }
