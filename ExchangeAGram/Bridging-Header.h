2af5c6acb7db83b9dcfef2d651981d317940872d
MaximeVDG <Max@MacBook-Air-de-Maxime.local>
2015-01-22 17:16:07 +0900

    added support for Facebook login and posting

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Maxime Vandegeerde on 22/01/15.
+//  Copyright (c) 2015 Maxime Vandegeerde. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
+
